89acb53a434a7134888039a6e646883b164eb45f
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2014-06-21 06:51:41 -0400

    Bump retransmit interval crudely to 2s

--- tcp.rkt
+++ tcp.rkt
@@ -310,7 +310,7 @@
 		    listener-listening?) ;; boolean
 	#:transparent)
 
-(define transmit-check-interval-msec 100)
+(define transmit-check-interval-msec 2000)
 (define inbound-buffer-limit 65535)
 (define maximum-segment-size 536) ;; bytes
 (define maximum-segment-lifetime-sec (* 2 60)) ;; two minutes; 2MSL is TIME-WAIT timeout
