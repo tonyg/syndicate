cdb9e1cb076e691ad95ef34815bb94d2a78561f6
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-03-06 13:58:36 +0000

    Chat client example.

---
+++ prospect/examples/chat-client.rkt
@@ -0,0 +1,26 @@
+#lang prospect
+
+(require (only-in racket/port read-bytes-line-evt))
+(require "../drivers/tcp.rkt")
+
+(define local-handle (tcp-handle 'chat))
+(define remote-handle (tcp-address "localhost" 5999))
+
+(spawn-tcp-driver)
+(spawn/stateless (lambda (e)
+                   (match e
+                     [(? patch/removed?) (quit)]
+                     [(message (at-meta (external-event _ (list (? eof-object?)))))
+                      (quit)]
+                     [(message (at-meta (external-event _ (list line))))
+                      (message (tcp-channel local-handle remote-handle line))]
+                     [(message (tcp-channel _ _ bs))
+                      (write-bytes bs)
+                      (flush-output)
+                      #f]
+                     [_ #f]))
+                 (sub (external-event (read-bytes-line-evt (current-input-port) 'any) ?)
+                      #:meta-level 1)
+                 (sub (tcp-channel remote-handle local-handle ?))
+                 (sub (advertise (tcp-channel remote-handle local-handle ?)))
+                 (pub (tcp-channel local-handle remote-handle ?)))
