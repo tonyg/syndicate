30d46a20194e2bcb6ef7982263f2f559fb6c3b27
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-12-11 20:22:36 +1300

    Avoid incorrect use of ?; replace with _

--- prospect/examples/actor/chat.rkt
+++ prospect/examples/actor/chat.rkt
@@ -19,14 +19,14 @@
          (define user (gensym 'user))
          (send-to-remote "Welcome, ~a.\n" user)
 
-         (until (retracted (advertise (tcp-channel them us ?)) #:meta-level 1)
+         (until (retracted (advertise (tcp-channel them us _)) #:meta-level 1)
                 (assert (present user))
                 (on (asserted (present $who)) (say who "arrived."))
                 (on (retracted (present $who)) (say who "departed."))
 
                 (on (message (says $who $what)) (say who "says: ~a" what))
 
-                (assert (advertise (tcp-channel us them ?)) #:meta-level 1)
+                (assert (advertise (tcp-channel us them _)) #:meta-level 1)
                 (on (message (tcp-channel them us $bs) #:meta-level 1)
                     (send! (says user (string-trim (bytes->string/utf-8 bs))))))))
 
@@ -36,6 +36,6 @@
  (%%boot
   (lambda ()
     (actor (define us (tcp-listener 5000))
-           (forever (assert (advertise (observe (tcp-channel ? us ?))) #:meta-level 1)
-                    (on (asserted (advertise (tcp-channel $them us ?)) #:meta-level 1)
+           (forever (assert (advertise (observe (tcp-channel _ us _))) #:meta-level 1)
+                    (on (asserted (advertise (tcp-channel $them us _)) #:meta-level 1)
                         (spawn-session them us)))))))
