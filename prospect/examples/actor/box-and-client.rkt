91c2d6a3c7bf1e96fbb57957526707d18c5ec739
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-12-11 17:55:46 +1300

    box-and-client actor example

---
+++ prospect/examples/actor/box-and-client.rkt
@@ -0,0 +1,22 @@
+#lang prospect
+;; Simple mutable box and count-to-infinity box client.
+
+(require prospect/actor)
+
+(struct set-box (new-value) #:transparent)
+(struct box-state (value) #:transparent)
+
+(%%boot
+ (lambda ()
+   (actor
+
+    (actor (forever #:collect [(current-value 0)]
+                    (assert (box-state current-value))
+                    (on (message (set-box $new-value))
+                        (log-info "box: taking on new-value ~v" new-value)
+                        new-value)))
+
+    (actor (forever (on (asserted (box-state $v))
+                        (log-info "client: learned that box's value is now ~v" v)
+                        (send! (set-box (+ v 1))))))
+    )))
