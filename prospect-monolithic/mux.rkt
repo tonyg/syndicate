tonyg/syndicate

commit de1dc5aa8e6546516bd18e30731e2b1238a08b6b

Fix "out"-related bug in monolithic implementation

prospect-monolithic/mux.rkt
@@ -74,7 +74,9 @@
   (values (for/list [(pid (tset->list affected-pids))]
             (cons pid (scn (biased-intersection new-routing-table (mux-interests-of new-m pid)))))
           (and (not (meta-label? label))
-               (drop-scn (scn (strip-interests new-routing-table))))))
+               (drop-scn (scn (trie-subtract (strip-interests new-routing-table)
+                                             (mux-interests-of new-m 'meta)
+                                             #:combiner (lambda (a b) #f)))))))
 
 (define (compute-affected-pids routing-table cover)
   (trie-match-trie cover

prospect-monolithic/examples/example-layer.rkt
@@ -1,0 +1,34 @@
+#lang prospect-monolithic
+;; Check that nested-world assertions are properly retracted.
+;; Should print two "Got SCN:" tries - one nonempty, and one empty.
+
+#;(spawn (lambda (e s)
+         (match e
+           [(message 'die) (quit)]
+           [_ #f]))
+       (void)
+       (scn/union
+        (subscription 'die)
+        (subscription (observe 'die))))
+
+(spawn-network
+ (spawn (lambda (e s)
+          (match e
+            [(message (at-meta 'die)) (quit)]
+            [_ #f]))
+        (void)
+        (scn/union
+         (subscription 'die #:meta-level 1)
+         (subscription (observe 'die) #:meta-level 1))))
+
+(spawn (lambda (e s)
+         (match e
+           [(scn g)
+            (printf "Got SCN:\n")
+            (pretty-print-trie g)
+            (transition s (if (trie-non-empty? g)
+                              (message 'die)
+                              '()))]
+           [_ #f]))
+       (void)
+       (scn (subscription (observe 'die))))

prospect/examples/example-layer.rkt
@@ -1,0 +1,32 @@
+#lang prospect
+;; Check that nested-world assertions are properly retracted.
+;; Should print two "Got SCN:" patches - one adding, and one removing (observe 'die).
+
+#;(spawn (lambda (e s)
+         (match e
+           [(message 'die) (quit)]
+           [_ #f]))
+       (void)
+       (patch-seq (sub 'die)
+                  (sub (observe 'die))))
+
+(spawn-network
+ (spawn (lambda (e s)
+          (match e
+            [(message (at-meta 'die)) (quit)]
+            [_ #f]))
+        (void)
+        (patch-seq (sub 'die #:meta-level 1)
+                   (sub (observe 'die) #:meta-level 1))))
+
+(spawn (lambda (e s)
+         (match e
+           [(? patch? p)
+            (printf "Got SCN:\n")
+            (pretty-print-patch p)
+            (transition s (if (patch/added? p)
+                              (message 'die)
+                              '()))]
+           [_ #f]))
+       (void)
+       (sub (observe 'die)))

commit 5f6f3429c8cd755cab36de51aa9df4c11d0e7052

Fix the fix for the "out"-related bug from commit de1dc5a.
The problem was that sometimes there'd be an assertion that `meta` had
asserted that *also* was asserted by some local process. Commit
de1dc5a introduced code that would drop the assertion in this case;
this commit changes it to only remove assertions from the set when
*only* `meta` is asserting them.

prospect-monolithic/mux.rkt
@@ -74,9 +74,9 @@
   (values (for/list [(pid (tset->list affected-pids))]
             (cons pid (scn (biased-intersection new-routing-table (mux-interests-of new-m pid)))))
           (and (not (meta-label? label))
-               (drop-scn (scn (trie-subtract (strip-interests new-routing-table)
-                                             (mux-interests-of new-m 'meta)
-                                             #:combiner (lambda (a b) #f)))))))
+               (drop-scn (scn (trie-relabel new-routing-table
+                                            (lambda (v)
+                                              (empty-tset-guard (tset-remove v 'meta)))))))))
 
 (define (compute-affected-pids routing-table cover)
   (trie-match-trie cover