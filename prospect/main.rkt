26966b9c34819e594f43b370966d7117590dd190
Sam Caldwell <samcaldwell@utexas.edu>
2016-02-02 18:06:16 -0500

    Implement trie-comprehension macros
    Trie comprehensions abstract the process of projecting one-or-more
    tries against some patterns, binding the results to usable variables,
    and iterating over the results.
    
    Currently supports for-trie/fold, for-trie/list, for-trie/set, and
    for-trie/patch, as well as the ability to easily create new trie
    comprehension macros. Note that these comprehensions operate in the
    style of `for*` rather than `for`.

--- prospect/main.rkt
+++ prospect/main.rkt
@@ -2,6 +2,8 @@
 
 (require "core.rkt")
 (require "ground.rkt")
+(require "comprehensions.rkt")
 
 (provide (all-from-out "core.rkt")
+         (all-from-out "comprehensions.rkt")
 	 (all-from-out "ground.rkt"))
