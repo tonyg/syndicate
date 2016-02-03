b85409ef10ce1c1c31d3139d71360c798a9bc1fc
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-02-02 23:01:08 -0500

    Demos of wildcard assertions

---
+++ prospect/examples/example-wildcard-assertion-2.rkt
@@ -0,0 +1,30 @@
+#lang prospect
+;; Demonstrate almost-wildcard assertions.
+;; One actor subscribes to everything - and so initially sees itself.
+;; The other advertises everything except subscriptions and at-meta assertions.
+;; The first actor's aggregate view of the network then includes everything
+;; except at-meta assertions.
+
+(require prospect/pretty)
+
+(spawn (lambda (e s)
+         (printf "Subscriber - Aggregate\n")
+         (prospect-pretty-print s)
+         (printf "Subscriber - Patch\n")
+         (prospect-pretty-print e)
+         (newline)
+         (if (patch? e)
+             (transition (update-interests s e) '())
+             #f))
+       (trie-empty)
+       (sub ?))
+
+(spawn (lambda (e s)
+         (printf "Asserter\n")
+         (prospect-pretty-print e)
+         (newline)
+         #f)
+       (void)
+       (patch-seq (assert ?)
+                  (retract (observe ?))
+                  (retract (at-meta ?))))
