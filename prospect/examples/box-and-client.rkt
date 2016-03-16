3c2995841e3b43e96df706ba8e45d807a8d4753a
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-03-16 13:13:04 -0400

    Use match-event in box-and-client example

--- prospect/examples/box-and-client.rkt
+++ prospect/examples/box-and-client.rkt
@@ -5,22 +5,20 @@
 (struct box-state (value) #:transparent)
 
 (spawn (lambda (e current-value)
-         (match e
+         (match-event e
            [(message (set-box new-value))
             (log-info "box: taking on new-value ~v" new-value)
             (transition new-value (patch-seq (retract (box-state current-value))
-                                             (assert (box-state new-value))))]
-           [_ #f]))
+                                             (assert (box-state new-value))))]))
        0
        (patch-seq (sub (set-box ?))
                   (assert (box-state 0))))
 
 (spawn (lambda (e s)
-         (match e
+         (match-event e
            [(patch added removed)
             (transition s (for-trie/list ([(box-state $v) added])
                             (log-info "client: learned that box's value is now ~v" v)
-                            (message (set-box (+ v 1)))))]
-           [_ #f]))
+                            (message (set-box (+ v 1)))))]))
        (void)
        (patch-seq (sub (box-state ?))))
