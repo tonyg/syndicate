7c4e00f614454a7bd765f8914df3c6f23b3dd039
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-05-09 12:55:20 -0400

    Command-line launcher for broker

--- racket/syndicate/info.rkt
+++ racket/syndicate/info.rkt
@@ -1,2 +1,4 @@
 #lang setup/infotab
 (define scribblings '(("scribblings/syndicate.scrbl" ())))
+(define racket-launcher-names '("syndicate-broker"))
+(define racket-launcher-libraries '("broker/server.rkt"))
