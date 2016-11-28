e7402e43879b7ffb9ebd26506db34398f1eb8fc6
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-11-29 10:16:39 +1300

    example-multiple-suspension-resumption.rkt

---
+++ racket/syndicate/examples/actor/example-multiple-suspension-resumption.rkt
@@ -0,0 +1,13 @@
+#lang syndicate/actor
+;; Shows that the checks enforcing single-use suspension-resumption are working.
+
+(actor #:name 'shouldnt-work
+       (field [k #f])
+       (on-start
+        (flush!)
+        (log-info "Result from suspension: ~v"
+                  (react/suspend (actual-k)
+                                 (on-start (k actual-k)
+                                           ((k) 'first-result))))
+        (flush!)
+        ((k) 'second-result)))
