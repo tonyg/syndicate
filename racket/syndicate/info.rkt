88d324929dcc9267d83f99a38a719619b51aa472
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2017-08-14 17:20:36 -0400

    Command-line tool, syndicate-render-msd

--- racket/syndicate/info.rkt
+++ racket/syndicate/info.rkt
@@ -1,5 +1,5 @@
 #lang setup/infotab
 (define scribblings '(("scribblings/syndicate.scrbl" ())))
-(define racket-launcher-names '("syndicate-broker"))
-(define racket-launcher-libraries '("broker/server.rkt"))
+(define racket-launcher-names '("syndicate-broker" "syndicate-render-msd"))
+(define racket-launcher-libraries '("broker/server.rkt" "trace/render-msd.rkt"))
 (define test-include-paths '("syndicate/tests"))
