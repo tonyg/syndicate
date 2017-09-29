db333a266f26808595d264f124a0f33e083797b6
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2017-09-29 17:34:40 +0100

    Tweaks to udp-echo.rkt

--- racket/syndicate/examples/actor/udp-echo.rkt
+++ racket/syndicate/examples/actor/udp-echo.rkt
@@ -1,7 +1,10 @@
 #lang syndicate
 
 (require/activate syndicate/drivers/udp)
+(require syndicate/protocol/advertise)
 
 (spawn (on (message (udp-packet $peer (udp-listener 5999) $body))
            (printf "Received from ~a: ~v\n" peer body)
-           (send! (udp-packet (udp-listener 5999) peer body))))
+           (send! (udp-packet (udp-listener 5999) peer body)))
+       (on (asserted (advertise (udp-packet _ (udp-listener 5999) _)))
+           (printf "Socket is ready and will forward datagrams.\n")))
