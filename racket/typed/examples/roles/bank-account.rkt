e876b0a16b5cd61bf9e79b709c586b333dbda25e
Sam Caldwell <samcaldwell@utexas.edu>
2018-07-25 17:26:47 -0400

    start on facet role types

---
+++ racket/typed/examples/roles/bank-account.rkt
@@ -0,0 +1,33 @@
+#lang typed/syndicate/roles
+
+(define-constructor (account balance)
+  #:type-constructor AccountT
+  #:with Account (AccountT Int)
+  #:with AccountRequest (AccountT ★/t))
+
+(define-constructor (deposit amount)
+  #:type-constructor DepositT
+  #:with Deposit (DepositT Int)
+  #:with DepositRequest (DepositT ★/t))
+
+(define-type-alias ds-type
+  (U Account
+     (Observe AccountRequest)
+     (Observe (Observe AccountRequest))
+     Deposit
+     (Observe DepositRequest)
+     (Observe (Observe DepositRequest))))
+
+(spawn ds-type
+  (print-role
+  (start-facet account-manager
+    (fields [balance Int 0])
+    (assert (account (ref balance)))
+    (on (asserted (deposit (bind amount Int)))
+        (set! balance (+ (ref balance) amount))))))
+
+(spawn ds-type
+  (start-facet observer
+    (fields)
+    (on (asserted (account (bind amount Int)))
+        (displayln amount))))
