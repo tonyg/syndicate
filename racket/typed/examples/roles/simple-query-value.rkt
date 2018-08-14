5b0bce7a124df385f2a78ee1550aa353bf57f207
Sam Caldwell <samcaldwell@utexas.edu>
2018-08-14 15:43:51 -0400

    query-value

---
+++ racket/typed/examples/roles/simple-query-value.rkt
@@ -0,0 +1,21 @@
+#lang typed/syndicate/roles
+
+;; Expected Output
+;; query: 0
+;; query: 19
+
+(define-type-alias ds-type
+  (U (Tuple String Int)
+     (Observe ★/t)))
+
+(dataspace ds-type
+  (spawn ds-type
+    (start-facet querier
+      (define/query-value key 0 (tuple "key" (bind v Int)) (+ v 1))
+      (assert (tuple "query" (ref key)))))
+  (spawn ds-type
+    (start-facet client
+      (assert (tuple "key" 18))
+      (during (tuple "query" (bind v Int))
+        (on start
+            (printf "query: ~v\n" v))))))
