13e988fe587f0af8ae26be099a3cbeee5c3b8979
Sam Caldwell <samc@ccs.neu.edu>
2019-05-23 16:43:39 -0400

    some work towards a better pattern language

--- racket/typed/core-expressions.rkt
+++ racket/typed/core-expressions.rkt
@@ -138,18 +138,20 @@
 (define-typed-syntax (match e [p s ...+] ...+) ≫
   [⊢ e ≫ e- (⇒ : τ-e)]
   #:fail-unless (pure? #'e-) "expression must be pure"
-  #:with (([x τ:type] ...) ...) (stx-map pat-bindings #'(p ...))
+  #:with (p/e ...) (for/list ([pat (in-syntax #'(p ...))])
+                     (elaborate-pattern/with-type pat #'τ-e))
+  #:with (([x τ:type] ...) ...) (stx-map pat-bindings #'(p/e ...))
   [[x ≫ x- : τ.norm] ... ⊢ (begin s ...) ≫ s- (⇒ : τ-s)
                 (⇒ ν-ep (~effs eps ...))
                 (⇒ ν-f (~effs fs ...))
                 (⇒ ν-s (~effs ss ...))] ...
   ;; REALLY not sure how to handle p/p-/p.match-pattern,
   ;; particularly w.r.t. typed terms that appear in p.match-pattern
-  [⊢ p ≫ p-- ⇒ τ-p] ...
+  [⊢ p/e ≫ p-- ⇒ τ-p] ...
   #:fail-unless (project-safe? #'τ-e (mk-U*- #'(τ-p ...))) "possibly unsafe pattern match"
   #:fail-unless (stx-andmap pure? #'(p-- ...)) "patterns must be pure"
   #:with (p- ...) (stx-map (lambda (p x-s xs) (substs x-s xs (compile-match-pattern p)))
-                           #'(p ...)
+                           #'(p/e ...)
                            #'((x- ...) ...)
                            #'((x ...) ...))
   --------------------------------------------------------------
@@ -200,13 +202,141 @@
     [(tuple p ...)
      #:with (([x:id τ:type] ...) ...) (stx-map pat-bindings #'(p ...))
      #'([x τ] ... ...)]
-    [(k:kons1 p)
+    #;[(k:kons1 p)
      (pat-bindings #'p)]
     [(~constructor-exp cons p ...)
      #:with (([x:id τ:type] ...) ...) (stx-map pat-bindings #'(p ...))
      #'([x τ] ... ...)]
     [_
      #'()]))
+
+(begin-for-syntax
+  ;; Any -> Bool
+  (define (dollar-variable? x)
+    (and (identifier? x)
+         (char=? (string-ref (symbol->string (syntax-e x)) 0) #\$)))
+
+  ;; dollar-id -> Identifier
+  (define (un-dollar x)
+    (datum->syntax x (string->symbol (substring (symbol->string (syntax-e x)) 1))))
+
+  (define-syntax-class dollar-id
+    #:attributes (id)
+    (pattern x:id
+             #:when (dollar-variable? #'x)
+             #:attr id (un-dollar #'x)))
+
+  ;; match things of the for "$X...:Y..." where X and Y are things without
+  ;; spaces (i.e. likely but not definitely legal identifiers)
+  (define DOLLAR-ANN-RX #px"^\\$(\\S*):(\\S*)$")
+
+  ;; Any -> RegexpMatchResults
+  (define (dollar-ann-variable? x)
+    (and (identifier? x)
+         (regexp-match DOLLAR-ANN-RX (symbol->string (syntax-e x)))))
+
+  (define-syntax-class dollar-ann-id
+    #:attributes (id ty)
+    (pattern x:id
+             #:do [(define match? (dollar-ann-variable? #'x))]
+             #:when match?
+             #:attr id (datum->syntax #'x (string->symbol (second match?)))
+             #:attr ty (datum->syntax #'x (string->symbol (third match?)))))
+
+  ;; expand uses of $ short-hand
+  ;; doesn't handle uses of $id or ($) w/o a type
+  (define (elaborate-pattern pat)
+    (syntax-parse pat
+      #:datum-literals (tuple _ $)
+      [_
+       #'discard]
+      [x:dollar-ann-id
+       (syntax/loc pat (bind x.id x.ty))]
+      [($ x:id ty)
+       (syntax/loc pat (bind x ty))]
+      [(tuple p ...)
+       (quasisyntax/loc pat
+         (tuple #,@(stx-map elaborate-pattern #'(p ...))))]
+      [(k:kons1 p)
+       (quasisyntax/loc pat
+         (k #,(elaborate-pattern #'p)))]
+      [(~constructor-exp ctor p ...)
+       (quasisyntax/loc pat
+         (ctor #,@(stx-map elaborate-pattern #'(p ...))))]
+      [e:expr
+       #'e]))
+
+  (define (elaborate-pattern/with-type pat ty)
+    (syntax-parse pat
+      #:datum-literals (tuple $)
+      [(~datum _)
+       #'discard]
+      [x:dollar-ann-id
+       (syntax/loc pat (bind x.id x.ty))]
+      [x:dollar-id
+       (quasisyntax/loc pat (bind x.id #,ty))]
+      [($ x:id ty)
+       (syntax/loc pat (bind x ty))]
+      [($ x:id)
+       (quasisyntax/loc pat (bind x #,ty))]
+      [(tuple p ...)
+       (define (matching? t)
+         (syntax-parse t
+           [(~Tuple tt ...)
+            #:when (stx-length=? #'(p ...) #'(tt ...))
+            #t]
+           [_ #f]))
+       (define selected
+         (syntax-parse ty
+           [tt
+            #:when (matching? ty)
+            #'tt]
+           [(~U* (~or (~and tt (~fail #:unless (matching? #'tt)))
+                      _) ...)
+            (mk-U- #'(tt ...))]))
+       (define (proj t i)
+         (syntax-parse t
+           [(~Tuple tt ...)
+            (stx-list-ref #'(tt ...) i)]
+           [(~U* tt ...)
+            (mk-U- (stx-map (lambda (x) (proj x i)) #'(tt ...)))]))
+       (define sub-pats
+         (for/list ([pat (in-syntax #'(p ...))]
+                    [i (in-naturals)])
+           (elaborate-pattern/with-type pat (proj selected i))))
+       (quasisyntax/loc pat
+         (tuple #,@sub-pats))]
+      [(~constructor-exp ctor p ...)
+       (define tag (ctor-type-tag #'ctor))
+       (define (matching? t)
+         (syntax-parse t
+           [(~constructor-type tag2 tt ...)
+            #:when (equal? tag (syntax-e #'tag2))
+            #:when (stx-length=? #'(p ...) #'(tt ...))
+            #t]
+           [_ #f]))
+       (define selected
+         (syntax-parse ty
+           [tt
+            #:when (matching? ty)
+            #'tt]
+           [(~U* (~or (~and tt (~fail #:unless (matching? #'tt)))
+                      _) ...)
+            (mk-U- #'(tt ...))]))
+       (define (proj t i)
+         (syntax-parse t
+           [(~constructor-type _ tt ...)
+            (stx-list-ref #'(tt ...) i)]
+           [(~U* tt ...)
+            (mk-U- (stx-map (lambda (x) (proj x i)) #'(tt ...)))]))
+       (define sub-pats
+         (for/list ([pat (in-syntax #'(p ...))]
+                    [i (in-naturals)])
+           (elaborate-pattern/with-type pat (proj selected i))))
+       (quasisyntax/loc pat
+         (ctor #,@sub-pats))]
+      [e:expr
+       #'e])))
 
 ;; TODO - figure out why this needs different list identifiers
 (define-for-syntax (compile-pattern pat list-binding bind-id-transformer exp-transformer)
@@ -216,7 +346,7 @@
         #:datum-literals (tuple discard bind)
         [(tuple p ...)
          #`(#,list-binding 'tuple #,@(stx-map loop #'(p ...)))]
-        [(k:kons1 p)
+        #;[(k:kons1 p)
          #`(#,(kons1->constructor #'k) #,(loop #'p))]
         [(bind x:id τ:type)
          (bind-id-transformer #'x)]
