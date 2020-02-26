01a544b0d930e4219f29d39524f90728c1e00bc9
Sam Caldwell <samc@ccs.neu.edu>
2020-02-26 14:50:35 -0500

    Allow `define`d expressions to have effects

---
+++ racket/typed/tests/regression-define-with-effects.rkt
@@ -0,0 +1,17 @@
+#lang typed/syndicate/roles
+
+(require rackunit/turnstile)
+
+(check-type
+ (begin
+  (field [boo Int 0])
+  (define x (begin (send! "hi") 5))
+  ;; relying on `set` not allowing effects for this to be a good test
+  (set! boo x)
+  3)
+ : Int)
+
+;; Used to get the error:
+; <pkgs>/syndicate/typed/tests/define-with-effects.rkt:10.2: set!: expression not allowed to have effects
+;   at: (set! boo x)
+;   in: (set! boo x)
