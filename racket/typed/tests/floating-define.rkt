9113d93acfe90f2d9cef77d033c31cf8f528a522
Sam Caldwell <samc@ccs.neu.edu>
2020-03-10 10:57:54 -0400

    floating define test

---
+++ racket/typed/tests/floating-define.rkt
@@ -0,0 +1,9 @@
+#lang typed/syndicate/roles
+
+(require rackunit/turnstile)
+
+;; TODO - currently fails with a racket error. Is that OK?
+;; (ideally, this would fail with a better error message)
+(typecheck-fail
+ (let ([x (define y 5)])
+   (add1 1)))
