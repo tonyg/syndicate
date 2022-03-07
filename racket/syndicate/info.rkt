fe798f72a10b9091a94e8c932ec40eee135e89b3
Sam Caldwell <samc@ccs.neu.edu>
2022-03-07 15:03:43 -0500

    untyped: omit a test that the package server does not like

--- racket/syndicate/info.rkt
+++ racket/syndicate/info.rkt
@@ -7,4 +7,7 @@
   '(;; Sam: example-plain is interactive, I think
     "examples/example-plain.rkt"
     ;; Sam: for whatever reason I get a failure to load libcrypto for f-to-c
-    "examples/actor/f-to-c.rkt"))
+    "examples/actor/f-to-c.rkt"
+    ;; Sam: this test displays to stderr which the package server does not like
+    "tests/nested-spawn-exceptions.rkt"
+    ))
