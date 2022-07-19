3d9b1c383c134f98490ea1899e4ecb6d85620477
Sam Caldwell <samc@ccs.neu.edu>
2022-07-19 09:32:10 -0400

    typed: update info.rkt

--- racket/typed/info.rkt
+++ racket/typed/info.rkt
@@ -9,4 +9,5 @@
 (define test-omit-paths
   ;; a number of the examples use SPIN for model checking which I need
   ;; to figure out how to get working on the package server
-  '("examples/"))
+  '("examples/")
+  '("tests/spin/"))
